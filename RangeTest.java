2d0cbd125c21a1a434b2a521ee9c51edda27b67a
mzafari2 <113462468+mzafari2@users.noreply.github.com>
2024-02-12 17:11:52 -0700

    added getCentralValue() and contains() tests.

--- RangeTest.java
+++ RangeTest.java
@@ -1,4 +1,4 @@
-
+import org.w3c.dom.ranges.Range;
 
 public class RangeTest {
 
@@ -57,7 +57,6 @@ public class RangeTest {
         assertTrue(obj.equals(new Range(1, 5)));
     }
 
-
     // Test cases for hashCode() method written by Maham Jamal (UCID: 30153574)
     @Test
     public void testHashCodeValidInput() {
@@ -87,10 +86,113 @@ public class RangeTest {
         assertEquals(hashCode1, hashCode2);
     }
 
-    @Test(expected = NullPointerException.class )
+    @Test(expected = NullPointerException.class)
     public void testHashCodeNullObject() {
         Range obj = null;
-         obj.hashCode();
-    
+        obj.hashCode();
+
     }
+
+    // Test cases for getCentralValue() method written by Mehrnaz Zafari (UCID:
+    // 30118953)
+
+    @Test
+    public void centralValueShouldBeThree() {
+        Range obj1 = new Range(1, 5);
+        assertEquals("The central value of 1 and 5 should be 3",
+                3, obj1.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueInvalidInput() {
+        Range obj1 = new Range(-1, 1);
+        assertNotEquals("The central value of -1 and 1 should not be 0.8",
+                0.8, obj1.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueOfSymmetricRangeShouldBeMidpoint() {
+        Range symmetricRange = new Range(-3, 3);
+        assertEquals("The central value of a symmetric range from -3 to 3 should be 0",
+                0, symmetricRange.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueOfAsymmetricRangeShouldBeMidpoint() {
+        Range symmetricRange = new Range(-2, 4);
+        assertEquals("The central value of a symmetric range from -2 to 4 should be 1",
+                1, symmetricRange.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueOfOddLenghtRange() {
+        Range oddLenghtRange = new Range(1, 6);
+        assertEquals("The central value of a symmetric range from -2 to 4 should be 1",
+                3.5, oddLenghtRange.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueOfEvenLenghtRange() {
+        Range oddLenghtRange = new Range(2, 6);
+        assertEquals("The central value of a symmetric range from 2 to 6 should be 4",
+                4, oddLenghtRange.getCentralValue(), .000000001d);
+    }
+
+    @Test
+    public void centralValueOfSinglePointRangeShouldBeThatValue() {
+        Range singlePointRange = new Range(5, 5);
+        assertEquals("The central value of a single-point range with value 5 should be 5",
+                5, singlePointRange.getCentralValue(), .000000001d);
+    }
+
+    // Test cases for contains() method written by Mehrnaz Zafari (UCID:
+    // 30118953)
+
+    @Test
+    public void shouldContainZero() {
+        Range obj1 = new Range(-1, 1);
+        assertTrue("The Range from -1 to 1 should contain", obj1.contains(0));
+    }
+
+    @Test
+    public void shouldContainCentralValue() {
+        Range obj1 = new Range(-1, 1);
+        double centralValue = obj1.getCentralValue();
+        assertTrue("The Range from -1 to 1 should contain", obj1.contains(centralValue));
+    }
+
+    @Test
+    public void shouldNotContainValueOutside() {
+        Range obj1 = new Range(-1, 1);
+        assertFalse("The Range from -1 to 1 should not contain", obj1.contains(-2.0));
+    }
+
+    @Test
+    public void shouldContainLowerBound() {
+        Range obj1 = new Range(0, 2);
+        double lowerBound = obj1.getLowerBound();
+        assertTrue("The Range from 0 to 2 should not contain", obj1.contains(lowerBound));
+    }
+
+    @Test
+    public void shouldContainUpperBound() {
+        Range obj1 = new Range(4, 5);
+        double upperBound = obj1.getUpperBound();
+        assertTrue("The Range from 4 to 5 should not contain", obj1.contains(upperBound));
+    }
+
+    @Test
+    public void shouldNotContainValueJustBelowLowerBound() {
+        Range obj1 = new Range(2, 4);
+        assertFalse("The Range from 2 to 4 should not contain a value just below its lower bound",
+                obj1.contains(obj1.getLowerBound() - 0.000001d));
+    }
+
+    @Test
+    public void shouldNotContainValueJustAboveUpperBound() {
+        Range obj1 = new Range(2, 4);
+        assertFalse("The Range from 2 to 4 should not contain a value just above its upper bound",
+                obj1.contains(obj1.getUpperBound() + 0.000001d));
+    }
+
 }
